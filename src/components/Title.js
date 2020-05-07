1d4858ab11d68619ed89f30d597ad939a6fecc82
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-05-07 12:29:54 -0500

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+    return (
+        <div className = "Title">
+            <header>
+             <h1>Bubble</h1>
+                <div className = "Title-Subtitle">
+                    <h1>Social Media made Social</h1>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Title
